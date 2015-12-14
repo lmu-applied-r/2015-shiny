0ef6c0e646a3bc46585dac4853b8e70d4c9732e5
Philipp Roesch <philipp.stats@gmail.com>
2015-12-14 19:45:31 +0100

    updates

--- R/server.R
+++ R/server.R
@@ -15,14 +15,14 @@ shinyServer(function(input, output) {
   # Generierung des oberen Outputs
   output$radio <- renderPlot({
     
-    # Einfügen der entsprechenden Bedingung für die Auswahl 'barplot()'
+    # Einfuegen der entsprechenden Bedingung fuer die Auswahl 'barplot()'
     if(input$radio==1){
       
       # Plot-Befehl
       barplot(table(cyl), col=c("lightblue1", "lightblue3", "lightblue4"), xlab="Anzahl Zylinder", main="Output Aufgabe 1")
     }
     
-    # Einfügen der entsprechenden Bedingung für die Auswahl 'pie()'
+    # Einfuegen der entsprechenden Bedingung fuer die Auswahl 'pie()'
     if(input$radio==2){
       pie(table(cyl), xlab="Anzahl Zylinder", col=c("ligh tblue1", "lightblue3", "lightblue4"), main="Output Aufgabe 1")
     }
@@ -34,25 +34,25 @@ shinyServer(function(input, output) {
   # Generierung des unteren Outputs
   output$radio2 <- renderPlot({
     
-    # Bedingter Code für 'hp'
+    # Bedingter Code fuer 'hp'
     if(input$radio2==1){
       plot(hp, main="Output Aufgabe 2")
     }
     
-    # Bedingter Code für 'mpg'
+    # Bedingter Code fuer 'mpg'
     if(input$radio2==2){
       plot(mpg, main="Output Aufgabe 2")
     }
     
-    # Bedingter Code für 'hp - mpg'
+    # Bedingter Code fuer 'hp - mpg'
     if(input$radio2==3){
       
-      # Bedingter Code für 'yes'
+      # Bedingter Code fuer 'yes'
       if(input$select1==1){
         plot(hp,mpg, xlab="Miles/(US) gallon", ylab="Gross horsepower", col=cyl+6, pch=cyl+11, main="Output Aufgabe 2", cex=1.5)
       }
       
-      # Bedingter Code für 'no'
+      # Bedingter Code fuer 'no'
       if(input$select1==2){
         plot(hp,mpg, xlab="Miles/(US) gallon", ylab="Gross horsepower",main="Output Aufgabe 2") 
       }
--- R/ui.R
+++ R/ui.R
@@ -16,7 +16,7 @@ shinyUI(fluidPage(
                               choices = list("hp" = 1, "mpg" = 2, "hp - mpg"=3),selected = 1),
                  
                  # Bedingtes Widget, welches erscheint bei der Auswahl
-                 # der 3. Möglichkeit in Aufgabe 2
+                 # der 3. Moeglichkeit in Aufgabe 2
                  conditionalPanel(condition = "input.radio2 ==3",
                                   selectInput("select1", label = h3("Getrennt nach 'cyl'?"),
                               choices = list("Yes" = 1, "No" = 2),selected = 2))),
